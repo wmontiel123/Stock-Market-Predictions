5ab4325718027183a683f7de79d462876096911b
Wmontiel <walterlorenzopy@hotmail.com>
2023-07-10 08:17:37 -0400

    FIRTS COMMIT

---
+++ main.py
@@ -0,0 +1,115 @@
+# Make sure that you have all these libaries available to run the code successfully
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime as dt
+import urllib.request, json
+import os
+import numpy as np
+import tensorflow as tf # This code has been tested with TensorFlow 1.6
+from sklearn.preprocessing import MinMaxScaler
+
+data_source = 'kaggle' # alphavantage or kaggle
+
+if data_source == 'alphavantage':
+    # ====================== Loading Data from Alpha Vantage ==================================
+
+    api_key = '378NCNF5N7VPBURT'
+
+    # American Airlines stock market prices
+    ticker = "AAL"
+
+    # JSON file with all the stock market data for AAL from the last 20 years
+    url_string = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&outputsize=full&apikey=%s"%(ticker,api_key)
+
+    # Save data to this file
+    file_to_save = 'stock_market_data-%s.csv'%ticker
+
+    # If you haven't already saved data,
+    # Go ahead and grab the data from the url
+    # And store date, low, high, volume, close, open values to a Pandas DataFrame
+    if not os.path.exists(file_to_save):
+        with urllib.request.urlopen(url_string) as url:
+            data = json.loads(url.read().decode())
+            # extract stock market data
+            data = data['Time Series (Daily)']
+            df = pd.DataFrame(columns=['Date','Low','High','Close','Open'])
+            for k,v in data.items():
+                date = dt.datetime.strptime(k, '%Y-%m-%d')
+                data_row = [date.date(),float(v['3. low']),float(v['2. high']),
+                            float(v['4. close']),float(v['1. open'])]
+                df.loc[-1,:] = data_row
+                df.index = df.index + 1
+        print('Data saved to : %s'%file_to_save)        
+        df.to_csv(file_to_save)
+    # If the data is already there, just load it from the CSV
+    else:
+        print('File already exists. Loading data from CSV')
+        df = pd.read_csv(file_to_save)
+
+else:
+
+    # ====================== Loading Data from Kaggle ==================================
+    # You will be using HP's data. Feel free to experiment with other data.
+    # But while doing so, be careful to have a large enough dataset and also pay attention to the data normalization
+    df = pd.read_csv(os.path.join('Stocks','hpq.us.txt'),delimiter=',',usecols=['Date','Open','High','Low','Close'])
+    print('Loaded data from the Kaggle repository')
+
+
+# Sort DataFrame by date
+df = df.sort_values('Date')
+
+# Double check the result
+df.head()
+
+
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
+plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500],rotation=45)
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Mid Price',fontsize=18)
+plt.show()
+
+
+# First calculate the mid prices from the highest and lowest
+high_prices = df.loc[:,'High'].to_numpy()
+low_prices = df.loc[:,'Low'].to_numpy()
+mid_prices = (high_prices+low_prices)/2.0
+
+train_data = mid_prices[:11000]
+test_data = mid_prices[11000:]
+
+# Scale the data to be between 0 and 1
+# When scaling remember! You normalize both test and train data with respect to training data
+# Because you are not supposed to have access to test data
+scaler = MinMaxScaler()
+train_data = train_data.reshape(-1,1)
+test_data = test_data.reshape(-1,1)
+
+
+# Train the Scaler with training data and smooth data
+smoothing_window_size = 2500
+for di in range(0,10000,smoothing_window_size):
+    scaler.fit(train_data[di:di+smoothing_window_size,:])
+    train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
+
+# You normalize the last bit of remaining data
+scaler.fit(train_data[di+smoothing_window_size:,:])
+train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
+
+# Reshape both train and test data
+train_data = train_data.reshape(-1)
+
+# Normalize test data
+test_data = scaler.transform(test_data).reshape(-1)
+
+# Now perform exponential moving average smoothing
+# So the data will have a smoother curve than the original ragged data
+EMA = 0.0
+gamma = 0.1
+for ti in range(11000):
+  EMA = gamma*train_data[ti] + (1-gamma)*EMA
+  train_data[ti] = EMA
+
+# Used for visualization and test purposes
+all_mid_data = np.concatenate([train_data,test_data],axis=0)
